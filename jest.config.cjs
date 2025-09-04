29889efc751c419c8e5e39bc473dbf6b6da0e815
Liz <liz.westapps@gmail.com>
2025-09-04 13:30:43 +0800

    test(jest): add coverage collection, reporters, and global thresholds

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,1 +1,1 @@
-module.exports={ testEnvironment:'node', roots:['<rootDir>/src'] }
+module.exports={ testEnvironment:'node', roots:['<rootDir>/src'], collectCoverageFrom:['src/**/*.js','!src/server.js'], coverageReporters:['text','lcov'], coverageThreshold:{ global:{ statements:80, branches:70, functions:50, lines:80 } } }
