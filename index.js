1458385067db815857bb15999b8475cfb1820b98
Liz <liz.westapps@gmail.com>
2025-09-09 18:16:07 +0800

    chore(eslint): move ignores to flat config; remove .eslintignore

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-nconst express = require('express');
+const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
